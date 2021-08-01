6d0b8896e07c39bc9332a0fc71b342ca4b7be266
ArtDark <balusoff@gmail.com>
2021-08-01 19:23:31 +0300

    Delete 01_rest/rpc directory

--- 01_rest/rpc/docker-entrypoint-initdb.d/00_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE offers
-(
-    id      BIGSERIAL PRIMARY KEY,
-    company TEXT      NOT NULL,
-    percent TEXT      NOT NULL,
-    comment TEXT      NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
